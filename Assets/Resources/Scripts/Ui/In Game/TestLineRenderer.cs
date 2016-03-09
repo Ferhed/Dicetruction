03acfee5f115106b8d49cb19f5cd7b37098f78b9
Cédric Brun <brun.cedric2233@gmail.com>
2016-03-09 18:07:18 +0100

    Re-mise en place du couvercle, twerk du LineRenderer

--- Assets/Resources/Scripts/Ui/In Game/TestLineRenderer.cs
+++ Assets/Resources/Scripts/Ui/In Game/TestLineRenderer.cs
@@ -19,13 +19,12 @@ public class TestLineRenderer : MonoBehaviour
         yield return new WaitForSeconds(0.3f);
 
         LineRenderer line = gameObject.GetComponent<LineRenderer>();
-        line.SetVertexCount(21);
+        line.SetVertexCount(105);
         line.SetWidth(0.1f, 0.1f);
         int index = 0;
-        for (float i = 0; i < 210; i += 10)
+        for (float i = 0; i < 210; i += 2)
         {
             line.SetPosition(index, new Vector3(3 , -(a * (i + b) * a * (i + b)) + c, i+5));
-
             index++;
         }
         finish = true;
