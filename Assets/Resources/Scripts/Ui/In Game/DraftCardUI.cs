ca4d4210878022512c7e23e5f67cc1acc1e32989
unknown <v.birembaut@J001-0133-07.etudiant.rubika-edu.com.local>
2016-03-10 10:03:27 +0100

    correction mana

--- Assets/Resources/Scripts/Ui/In Game/DraftCardUI.cs
+++ Assets/Resources/Scripts/Ui/In Game/DraftCardUI.cs
@@ -15,6 +15,11 @@ public class DraftCardUI : MonoBehaviour
 				m_uiImage = GetComponent<Image> ();
 			if (value.Image != null)
 				m_uiImage.sprite = value.Image;
+			if (m_activeCard.GetEnergy () != 0) {
+				m_costText.text = m_activeCard.GetEnergy ().ToString ();
+				m_costText.gameObject.SetActive (true);
+			} else
+				m_costText.gameObject.SetActive (false);
 		}
 	}
 
@@ -29,6 +34,8 @@ public class DraftCardUI : MonoBehaviour
 
 	private Image m_uiImage;
 	private bool m_selected = false;
+	[SerializeField]
+	private Text m_costText;
 
 	void Awake ()
 	{
@@ -50,20 +57,20 @@ public class DraftCardUI : MonoBehaviour
 	public void OnSelect ()
 	{
 		transform.DOScale (1.2f, 0.2f).SetEase (Ease.InOutSine);
-		GetComponent<Image> ().DOColor (new Color (0.75f, 0.75f, 0.75f, 1), 0.2f).SetEase (Ease.InOutSine);
+		GetComponent<Image> ().DOColor (Color.white, 0.2f).SetEase (Ease.InOutSine);
 	}
 
 	public void OnDeselect ()
 	{
 		transform.DOScale (1, 0.2f).SetEase (Ease.InOutSine);
-		GetComponent<Image> ().DOColor (Color.white, 0.2f).SetEase (Ease.InOutSine);
+		GetComponent<Image> ().DOColor (new Color (137f / 255f, 137f / 255f, 137f / 255f, 1), 0.2f).SetEase (Ease.InOutSine);
 	}
 
 	public void OnPressed ()
 	{
 		if (m_selected) {
-			Ui_Manager.Instance.DeselectSpell (m_activeCard.GetEnergy());
-		} else if (!Ui_Manager.Instance.SelectSpell (m_activeCard.GetEnergy())) {
+			Ui_Manager.Instance.DeselectSpell (m_activeCard.GetEnergy ());
+		} else if (!Ui_Manager.Instance.SelectSpell (m_activeCard.GetEnergy ())) {
 			return;
 		}
 		transform.DOMoveY ((m_selected) ? -50 : 50, 0.2f).SetRelative ().SetEase (Ease.InOutSine);
